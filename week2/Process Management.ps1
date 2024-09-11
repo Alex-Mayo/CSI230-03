90e683ffbb49bfeb1f4c4e40a9cc52ad9eed5735
Alex <alexander.mayo@mymail.champlain.edu>
2024-09-11 16:29:45 -0400

    Process Management

---
+++ week2/Process Management.ps1
@@ -0,0 +1,10 @@
+﻿Get-Process -Name "C*"
+
+Get-Process | Where-Object { $_.Path -notcontains "*system32*" }
+
+Get-Service | Where-Object { $_.Status -eq "Stopped" } | Sort-Object | Export-Csv -Path "C:\Users\champuser\CSI230-03\week2\services.csv"
+
+$ChromePath = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+$StartPage = "champlain.edu"
+
+if(Get-Process chrome) { Stop-Process -processname chrome } else { Start-Process -FilePath $ChromePath -ArgumentList $StartPage }
