d2425a040abc38d4e0e974faee5969a7eea16e42
Alex <alexander.mayo@mymail.champlain.edu>
2024-09-11 15:33:49 -0400

    PowerShell Basic Lab 1

---
+++ week2/PowerShell Basic Lab - 1.ps1
@@ -0,0 +1,32 @@
+﻿cd $PSScriptRoot
+
+$files=(Get-ChildItem)
+for( $j = 0; $j -le $files.Length; $j++){
+
+    if($files[$j].Name -ilike "*ps1"){
+        Write-Host $files[$j].Name
+    }
+}
+
+
+$folderpath = "$PSScriptRoot\outfolder"
+if (Test-Path $folderpath){
+    Write-Host "Folder Already Exists"   
+}
+else{
+    New-Item -Name "outfolder" -ItemType "directory" -Path $folderpath
+}
+
+
+cd $PSScriptRoot
+$files=(Get-ChildItem)
+
+$folderPath = "$PSScriptRoot/outfolder/"
+$filePath = Join-Path -Path $folderPath "out.csv"
+
+$files |  Where-Object { $_.Extension -eq ".ps1" } | Export-Csv -Path $filePath
+
+
+$files =Get-ChildItem -Recurse -File
+$files | Rename-Item -NewName { $_.Name -replace '.csv', '.log' }
+Get-ChildItem -Recurse -File
