712210a26249e014a236b74419478b3d0ac8c4fd
Alex <alexander.mayo@mymail.champlain.edu>
2024-10-01 22:05:27 -0400

    Parsing Apache Logs

---
+++ week4/Parsing-Apache-Logs.ps1
@@ -0,0 +1,21 @@
+﻿function ApacheLogs1(){
+    $logsNotformatted = Get-Content C:\xampp\apache\logs\access.log
+    $tableRecords = @()
+
+    for($i = 0; $i -lt $logsNotformatted.Count; $i++){
+        $words = $logsNotformatted[$i].Split(" ")
+
+        $tableRecords += [pscustomobject]@{ "IP" = $words[0]; `
+                                            "Time" = $words[1].Trim('['); `
+                                            "Method" = $words[2].Trim('"'); `
+                                            "Page" = $words[3]; `
+                                            "Protocol" = $words[4]; `
+                                            "Response" = $words[5]; `
+                                            "Referrer" = $words[6]; `
+                                            "Client" = $words[11..($words.Length)]; }
+    }
+    return $tableRecords | Where-Object { $_.IP -ilike "10.*" }
+}
+
+$tableRecords = ApacheLogs1
+$tableRecords | Format-Table -AutoSize -Wrap
--- week4/main.ps1
+++ week4/main.ps1
@@ -1,5 +1,9 @@
 ﻿. (Join-Path $PSScriptRoot Apache-Logs.ps1)
+. (Join-Path $PSScriptRoot Parsing-Apache-Logs.ps1)
 
 $ipsoftens = getVisitedIpAddresses "index.html" ' 200 ' "Chrome"
 $counts = $ipsoftens | Group-Object IP
 $counts | Select-Object Count, Name
+
+$tableRecords = ApacheLogs1
+$tableRecords | Format-Table -AutoSize -Wrap
