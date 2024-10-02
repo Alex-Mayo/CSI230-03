1b6a22268cde97ec30dd26bc84cd77640a336104
Alex <alexander.mayo@mymail.champlain.edu>
2024-10-01 21:42:08 -0400

    Windows Apache Logs

---
+++ week4/Apache-Logs.ps1
@@ -0,0 +1,16 @@
+﻿function getVisitedIpAddresses($page, $HTTPCode, $WebBrowser){
+
+    $Connections = Get-Content C:\xampp\apache\logs\access.log | Select-String $page, $HTTPCode, $WebBrowser
+
+    $regex = [regex] "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+
+    $IpAddresses = $regex.Matches($Connections)
+
+    $ips = @()
+
+    for($i = 0; $i -lt $IpAddresses.Count; $i++){
+        $ips += [pscustomobject]@{ "IP" = $IpAddresses[$i].Value}
+    }
+
+    return $ips
+}
---
+++ week4/main.ps1
@@ -0,0 +1,5 @@
+﻿. (Join-Path $PSScriptRoot Apache-Logs.ps1)
+
+$ipsoftens = getVisitedIpAddresses "index.html" ' 200 ' "Chrome"
+$counts = $ipsoftens | Group-Object IP
+$counts | Select-Object Count, Name
