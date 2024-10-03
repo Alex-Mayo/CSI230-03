4bc53dcc514d592e0e91ff9484dd01e6f4292b17
Alex <alexander.mayo@mymail.champlain.edu>
2024-10-03 13:41:00 -0400

    HTML Elements and Web Requests

---
+++ week5/HTML Elements and Web Requests.ps1
@@ -0,0 +1,12 @@
+﻿$scraped_page = Invoke-WebRequest -TimeoutSec 10 http://10.0.17.21/ToBeScraped.html
+
+#$scraped_page.Links.Count
+#$scraped_page.Links
+
+#$scraped_page.Links | Select-Object href, outerText
+
+#$h2s = $scraped_page.ParsedHtml.body.getElementsByTagName("h2") | Select-Object outerText
+#$h2s
+
+$divs1 = $scraped_page.ParsedHtml.body.getElementsByTagName("div") | Where { $_.getAttributeNode("class").Value -ilike "div-1"} | select innerText
+$divs1
